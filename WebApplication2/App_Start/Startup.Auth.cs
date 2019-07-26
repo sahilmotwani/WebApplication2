09f7253f576216d98c427362244a397a9de97311
Sahil Motwani <samotwani@deloitte.com>
2019-07-26 09:06:18 -0500

    Initiatl commit

---
+++ WebApplication2/App_Start/Startup.Auth.cs
@@ -0,0 +1,25 @@
+﻿using Owin;
+using Microsoft.Owin.Security;
+using System.IdentityModel.Tokens;
+using System.Configuration;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApplication2
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Tenant = ConfigurationManager.AppSettings["Tenant"],
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = ConfigurationManager.AppSettings["Audience"]
+                    }
+                });
+        }
+    }
+}
